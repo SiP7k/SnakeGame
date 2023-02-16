03589853fe6f4ef84c3a34d419a1b1c733c75356
Савва Чальцев <cavvach@gmail.com>
2023-02-16 14:10:23 -0300

    Moved TestingProject in public repos

---
+++ SnakeGame.Tests2/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace SnakeGame.Tests2
+{
+    [TestFixture]
+    internal class CalculatorTests
+    {
+        Calculator calculator = new Calculator();
+
+        [Test]
+        public void AdditionalMustReturnValidValue()
+        {
+            Assert.That(calculator.Additional(5, 10) == 15);
+        }
+        [Test]
+        public void MultiplicationMustReturnInvalidValue() 
+        {
+            Assert.That(calculator.Multiplication(5, 10) == 50);
+        }
+        [Test]
+        public void SubtractionShouldReturnValidValue()
+        {
+            Assert.That(calculator.Subtraction(10, 5) == 5);
+        }
+        [Test]
+        public void DivisionShouldReturnValidValue()
+        {
+            Assert.That(calculator.Division(10,5) == 2);
+        }
+
+    }
+}
